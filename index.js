eedd60e0ce67cb2cb7dc3a011a0ed43b554075bb
Bernardo F. B. Braga <bernardo.braga@intelie.com.br>
2021-01-09 20:24:21 -0300

    create the floater element

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
+import Floater from './floater.js'
+const DEBUG = true
+
+function debug(s) {
+    if (DEBUG) console.log(s)
+}
+
+
 window.addEventListener('DOMContentLoaded', () => {
-    const DEBUG = true
-
-    function debug(s) {
-        if (DEBUG) console.log(s)
-    }
-
     const canvas = document.getElementById('canvas')
     const ctx = canvas.getContext('2d');
 
-    const seed = [128, 128, 128]
-    function tick() {
-        seed.forEach((v, i, a) => {
-            const factor = 30
-            const delta = ((Math.random() * 2) - 1) * factor
-            a[i] = Math.min(Math.abs(Math.round(v + delta)),255)
-        })
-       const [r,g,b] = seed
-        ctx.fillStyle = `rgb(${r},${g},${b})`;
-        ctx.fillRect(canvas.width/3,canvas.height/3, canvas.width/3, canvas.height/3);
+    const floater = new Floater();
+    function tick(){
+        floater.draw(ctx)
     }
 
     let lastTick
@@ -30,5 +25,5 @@ window.addEventListener('DOMContentLoaded', () => {
             tick()
             lastTick = undefined
         })
-    }, 2000) // 1,666 for 60fps
+    }, 17) // 16,666 for 60fps
 })
---
+++ readme.md
@@ -0,0 +1,4 @@
+This experiment intends to show a few things:
+1- How to set up a super slim experiment (in the age of create-react-app)
+2- How to manipulate a canvas element
+3- How to set up something similar to a game engine to update the screen
